679778c0ee492ca9ba07129e6184585b20653aeb
Maria del Mar García <Maria del Mar Garca>
2016-12-09 17:04:05 +0100

    arreglados todos los fallos de librerias, y eliminado el duplicado.

--- Museo/app/src/main/java/com/example/ferch/museo/Principal.java
+++ Museo/app/src/main/java/com/example/ferch/museo/Principal.java
@@ -1,20 +1,72 @@
 package com.example.ferch.museo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Principal extends BasicActivity{
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+public class Principal extends AppCompatActivity implements View.OnClickListener{
 
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        //Se Instancia el botón de Scan
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        //Se Instancia el Campo de Texto para el nombre del formato de código de barra
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        //Se Instancia el Campo de Texto para el contenido  del código de barra
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        //Se agrega la clase MainActivity.java como Listener del evento click del botón de Scan
+        scanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Se responde al evento click
+        if(view.getId()==R.id.scan_button) {
+            //Se instancia un objeto de la clase IntentIntegrator
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            //Se procede con el proceso de scaneo
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //Se obtiene el resultado del proceso de scaneo y se parsea
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            //Quiere decir que se obtuvo resultado pro lo tanto:
+            //Desplegamos en pantalla el contenido del código de barra scaneado
+            String scanContent = scanningResult.getContents();
+            contentTxt.setText("Contenido: " + scanContent);
+            //Desplegamos en pantalla el nombre del formato del código de barra scaneado
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("Formato: " + scanFormat);
+        }else{
+            //Quiere decir que NO se obtuvo resultado
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No se ha recibido datos del scaneo!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+   /* @Override
+    public boolean onCreateOptionMenu(Menu menu){
+        //Inflate the menu this adds intems to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }*/
 }
